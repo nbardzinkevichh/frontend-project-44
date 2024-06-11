714de7ddf22817e6f0d287aa332e5423e74898c5
nikolai.b <nbardzinkevichh@gmail.com>
2024-06-12 06:32:14 +0700

    add brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import greetPlayer from '../src/cli.js';
+import {
+  askQuestion, getRandomNumber, isAnswerCorrect, gameFinish,
+} from '../src/index.js';
+
+console.log('What number is missing in the progression?');
+
+const getProgression = (randomStep) => {
+  const progression = [];
+
+  for (let i = 0; i < 110; i += randomStep) {
+    progression.push(i);
+    if (progression.length === 10) break;
+  }
+
+  const answer = progression[getRandomNumber(10)];
+  const modifiedProgression = progression.join(' ').replace(answer, '..').split(' ');
+  
+  return [modifiedProgression, answer];
+}
+
+let correctAnswers = 0;
+let i = 0;
+while (i < 3) {
+  let randomStep = getRandomNumber(10);
+  randomStep = getRandomNumber(2) === 0 ? -randomStep : randomStep;
+  const [progression, answer] = getProgression(randomStep);
+  console.log(...progression); 
+  const question = askQuestion(`Your answer:\n`);
+
+  if (!isAnswerCorrect(question, answer.toString())) {
+    break;
+  } else {
+    correctAnswers += 1;
+  }
+
+  i += 1;
+}
+
+gameFinish(correctAnswers);
