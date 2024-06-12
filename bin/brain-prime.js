44dc64a9a534b7d828e2815f9b1d4720d4000034
nikolai.b <nbardzinkevichh@gmail.com>
2024-06-12 19:33:07 +0700

    change logic in brain-prime.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -8,22 +8,26 @@ console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 let correctAnswers = 0;
 let i = 0;
-let j = 1;
 
-while (i < 3) {
-  const randomNumber = getRandomNumber(200);
-  let answer = 0;
+const isPrime = (number) => {
   let divisors = 0;
-  while (j <= 5) {
-    if (randomNumber % j === 0) {
+  let j = 0;
+  while (j < 10) {
+    if (number % j === 0) {
       divisors += 1;
     }
     j += 1;
   }
+  return divisors === 1 ? 'yes' : 'no';
+}
 
-  answer = divisors === 1 ? 'yes' : 'no';
+while (i < 3) {
+  const randomNumber = getRandomNumber(200);
+  let answer = 0;
+
+  answer = isPrime(randomNumber);
   console.log(answer);
-  const question = askQuestion(`Question: ${randomNumber}\n`);
+  const question = askQuestion(`Question: ${randomNumber}\nYour answer: `);
 
   if (!isAnswerCorrect(question, answer)) {
     break;
